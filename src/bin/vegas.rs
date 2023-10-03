0ade2ec08f32fcd73a611bd37053cc4124526ad4
Akshay Narayan <akshayn@icsi.berkeley.edu>
2023-10-03 12:01:08 -0700

    update for new CCP version

--- src/bin/vegas.rs
+++ src/bin/vegas.rs
@@ -1,93 +1,51 @@
-extern crate clap;
-use clap::Arg;
-extern crate time;
-#[macro_use]
-extern crate slog;
-
-extern crate ccp_vegas;
-extern crate portus;
-
-use ccp_vegas::Vegas;
-use portus::ipc::{BackendBuilder, Blocking};
+use clap::{builder::Command, Arg};
+use portus::Error;
+use tracing::{info, warn};
 
 fn make_args() -> Result<(ccp_vegas::VegasConfig, String), String> {
-    let matches = clap::App::new("CCP Vegas")
+    let matches = Command::new("CCP Vegas")
         .version("0.1.0")
         .author("Frank Cangialosi <frankc@csail.mit.edu>")
         .about("Implementation of Vegas congestion control")
-        .arg(Arg::with_name("ipc")
-             .long("ipc")
-             .help("Sets the type of ipc to use: (netlink|unix|char)")
-             .default_value("unix")
-             .validator(portus::algs::ipc_valid))
-        .arg(Arg::with_name("alpha")
-             .long("alpha")
-             .help("Increase cwnd if estimate of packets in queue <= alpha")
-             .default_value("2"))
-        .arg(Arg::with_name("beta")
-             .long("beta")
-             .help("Decrease cwnd if estimate of packets in queue >= beta")
-             .default_value("4"))
+        .arg(
+            Arg::new("ipc")
+                .long("ipc")
+                .help("Sets the type of ipc to use: (netlink|unix|char)")
+                .default_value("unix")
+                .value_parser(|s: &str| match portus::algs::ipc_valid(s.to_owned()) {
+                    Ok(_) => Ok(s.to_owned()),
+                    Err(err) => Err(err),
+                }),
+        )
+        .arg(
+            Arg::new("alpha")
+                .long("alpha")
+                .help("Increase cwnd if estimate of packets in queue <= alpha")
+                .default_value("2"),
+        )
+        .arg(
+            Arg::new("beta")
+                .long("beta")
+                .help("Decrease cwnd if estimate of packets in queue >= beta")
+                .default_value("4"),
+        )
         .get_matches();
 
     Ok((
         ccp_vegas::VegasConfig {
-            alpha: matches.value_of("alpha").unwrap().parse::<u32>().unwrap(),
-            beta: matches.value_of("beta").unwrap().parse::<u32>().unwrap(),
+            alpha: *matches.get_one("alpha").unwrap(),
+            beta: *matches.get_one("beta").unwrap(),
         },
-        String::from(matches.value_of("ipc").unwrap()),
+        matches.get_one::<String>("ipc").unwrap().to_owned(),
     ))
 }
 
-fn main() {
-    let log = portus::algs::make_logger();
+fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
     let (cfg, ipc) = make_args()
-        .map_err(|e| warn!(log, "bad argument"; "err" => ?e))
+        .map_err(|err| warn!(?err, "bad argument"))
         .unwrap_or_default();
 
-    match ipc.as_str() {
-        "unix" => {
-            use portus::ipc::unix::Socket;
-            let b = Socket::<Blocking>::new("in", "out")
-                .map(|sk| BackendBuilder{sock: sk})
-                .expect("unix ipc initialization");
-            portus::run::<_, Vegas<_>>(
-                b,
-                &portus::Config {
-                    logger: Some(log),
-                    config: cfg,
-                }
-            ).unwrap();
-        }
-        #[cfg(all(target_os = "linux"))]
-        "netlink" => {
-            use portus::ipc::netlink::Socket;
-            let b = Socket::<Blocking>::new()
-                .map(|sk| BackendBuilder{sock: sk})
-                .expect("netlink ipc initialization");
-            portus::run::<_, Vegas<_>>(
-                b,
-                &portus::Config {
-                    logger: Some(log),
-                    config: cfg,
-                }
-            ).unwrap();
-        }
-        #[cfg(all(target_os = "linux"))]
-        "char" => {
-            use portus::ipc::kp::Socket;
-            let b = Socket::<Blocking>::new()
-                .map(|sk| BackendBuilder {sock: sk})
-                .expect("char ipc initialization");
-            portus::run::<_, Vegas<_>>(
-                b,
-                &portus::Config {
-                    logger: Some(log),
-                    config: cfg,
-                }
-            ).unwrap()
-        }
-        _ => unreachable!(),
-    }
-            
+    info!(algorithm = "Vegas", ?ipc, ?cfg, "starting vegas-ccp");
+    portus::start!(ipc.as_str(), cfg)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,33 @@
-#[macro_use]
-extern crate slog;
-extern crate time;
-extern crate portus;
-
-use portus::{CongAlg, Config, Datapath, DatapathInfo, DatapathTrait, Report};
 use portus::ipc::Ipc;
 use portus::lang::Scope;
+use portus::{CongAlg, Datapath, DatapathInfo, DatapathTrait, Flow, Report};
+use std::collections::HashMap;
+use tracing::{info, warn};
 
-const INIT_CWND_PKTS: u32 = 10;
-
-pub struct Vegas<T: Ipc> {
-    control_channel: Datapath<T>,
-    logger: Option<slog::Logger>,
-    sc: Scope,
-    mss: u32,
-    cwnd: u32,
-    min_rtt: f32,
-    alpha: u32,
-    beta: u32,
-}
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct VegasConfig {
     pub alpha: u32,
-    pub beta:  u32,
+    pub beta: u32,
 }
 
 impl Default for VegasConfig {
     fn default() -> Self {
-        VegasConfig {
-            alpha: 2,
-            beta: 4,
-        }
+        VegasConfig { alpha: 2, beta: 4 }
     }
 }
 
-impl<T: Ipc> Vegas<T> {
-    fn update_cwnd(&self) {
-        if let Err(e) = self.control_channel
-            .update_field(&self.sc, &[("Cwnd", self.cwnd)]) 
-        {
-            self.logger.as_ref().map(|log| {
-                warn!(log, "Cwnd update error";
-                      "err" => ?e,
-                );
-            });
-        }
+impl<T: Ipc> CongAlg<T> for VegasConfig {
+    type Flow = Vegas<T>;
+
+    fn name() -> &'static str {
+        "vegas"
     }
 
-    fn install_program(&self) -> Scope {
-        self.control_channel.install(
-            b"
+    fn datapath_programs(&self) -> HashMap<&'static str, String> {
+        let mut h = HashMap::default();
+        h.insert(
+            "vegas",
+            "
                 (def (Report
                         (volatile acked 0)
                         (volatile rtt 0)
@@ -63,45 +41,61 @@ impl<T: Ipc> Vegas<T> {
                     (report)
                     (:= Micros 0)
                 )
-            ", None
-        ).unwrap()
+            "
+            .to_owned(),
+        );
+        h
+    }
+
+    fn new_flow(&self, mut control: Datapath<T>, info: DatapathInfo) -> Self::Flow {
+        let sc = control
+            .set_program("vegas", None)
+            .expect("set vegas datapath program");
+        Vegas {
+            control_channel: control,
+            alpha: self.alpha,
+            beta: self.beta,
+            sc,
+            mss: info.mss,
+            cwnd: info.init_cwnd,
+            min_rtt: f32::MAX,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Vegas<T: Ipc> {
+    control_channel: Datapath<T>,
+    alpha: u32,
+    beta: u32,
+    sc: Scope,
+    mss: u32,
+    cwnd: u32,
+    min_rtt: f32,
+}
+
+impl<T: Ipc> Vegas<T> {
+    fn update_cwnd(&self) {
+        if let Err(err) = self
+            .control_channel
+            .update_field(&self.sc, &[("Cwnd", self.cwnd)])
+        {
+            warn!(?err, "Cwnd update error");
+        }
     }
 
     fn get_fields(&mut self, m: &Report) -> Option<(u32, u32)> {
-        let acked = m.get_field(&String::from("Report.acked"), &self.sc).expect(
-            "expected acked field in returned measurement",
-        ) as u32;
-        let rtt = m.get_field(&String::from("Report.rtt"), &self.sc).expect(
-            "expected rtt field in returned measurement",
-        ) as u32;
+        let acked = m
+            .get_field(&String::from("Report.acked"), &self.sc)
+            .expect("expected acked field in returned measurement") as u32;
+        let rtt = m
+            .get_field(&String::from("Report.rtt"), &self.sc)
+            .expect("expected rtt field in returned measurement") as u32;
         Some((acked, rtt))
     }
-
 }
 
-impl<T: Ipc> CongAlg<T> for Vegas<T> {
-    type Config = VegasConfig;
-
-    fn name() -> String {
-        String::from("vegas")
-    }
-    
-    fn create(control: Datapath<T>, cfg: Config<T, Vegas<T>>, info: DatapathInfo) -> Self {
-        let mut s = Self {
-            control_channel: control,
-            logger: cfg.logger,
-            sc: Scope::new(),
-            mss: info.mss,
-            cwnd: INIT_CWND_PKTS * info.mss,
-            min_rtt: 0.0,
-            alpha: cfg.config.alpha,
-            beta: cfg.config.beta,
-        };
-        
-        s.sc = s.install_program();
-        s
-    }
-
+impl<T: Ipc> Flow for Vegas<T> {
     fn on_report(&mut self, _sock_id: u32, r: Report) {
         let fields = self.get_fields(&r);
         if fields.is_none() {
@@ -109,12 +103,13 @@ impl<T: Ipc> CongAlg<T> for Vegas<T> {
         }
         let (bytes_acked, rtt_us) = fields.unwrap();
         let curr_rtt = (rtt_us as f32) / 1_000_000.0;
-        
-        if self.min_rtt <= 0.0 || curr_rtt < self.min_rtt {
+
+        if curr_rtt < self.min_rtt {
             self.min_rtt = curr_rtt;
         }
 
-        let in_queue = ((self.cwnd as f32) * (curr_rtt - self.min_rtt) / (curr_rtt * self.mss as f32)) as u32;
+        let in_queue =
+            ((self.cwnd as f32) * (curr_rtt - self.min_rtt) / (curr_rtt * self.mss as f32)) as u32;
         if in_queue <= self.alpha {
             self.cwnd += self.mss;
         } else if in_queue >= self.beta {
@@ -122,15 +117,6 @@ impl<T: Ipc> CongAlg<T> for Vegas<T> {
         }
 
         self.update_cwnd();
-
-        self.logger.as_ref().map(|log| {
-            info!(log, "got report";
-                  "bytesAcked" => bytes_acked,
-                  "curr_rtt" => curr_rtt,
-                  "min_rtt" => self.min_rtt,
-                  "in_queue" => in_queue,
-                  "cwnd" => self.cwnd,
-            );   
-        });
+        info!(?bytes_acked, ?curr_rtt, ?in_queue, ?self.cwnd, ?self.min_rtt, "got report");
     }
 }
